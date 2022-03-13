96243765ddfc077f4eee98aac5c93516669cceb3
Hyper Jones <47111201+hyperjones@users.noreply.github.com>
2022-03-13 12:35:59 +0800

    Update 1.c

--- CBasicDeclarations&Expressions/1.c
+++ CBasicDeclarations&Expressions/1.c
@@ -1,1 +1,20 @@
 
+#include <stdio.h>
+
+int main()
+{
+    printf("Name\t: Jones Sheesh"
+           "\nDOB\t: September 23, 1975"
+           "\nMobile\t: 99-9999999");
+    return 0;
+}
+
+/**************************************************************************
+Write a C program to print your name, date of birth. and mobile number.
+Go to the editor.
+Expected Output:
+
+Name   : Alexandra Abramov
+DOB    : July 14, 1975
+Mobile : 99-9999999999
+****************************************************************************/
